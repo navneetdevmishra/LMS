35d7a2bcbc80440fac417e63ef3980ad3d1a9b7b
Navneet Mishra <161726698+navneetdevmishra@users.noreply.github.com>
2025-04-04 20:39:56 +0530

    Update settings.py

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CSRF_TRUSTED_ORIGINS = ['https://lms-1enw.onrender.com']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
